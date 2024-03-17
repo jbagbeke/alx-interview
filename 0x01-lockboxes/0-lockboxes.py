49fa16fbdc9913044d3534fc97e16c21c94415b4
Joshua Bubune <joshua.legithive@gmail.com>
2024-03-17 18:35:34 +0000

    0x01. Lockboxes Mandatory Task 0

--- 0x01-lockboxes/0-lockboxes.py
+++ 0x01-lockboxes/0-lockboxes.py
@@ -11,7 +11,7 @@ def canUnlockAll(boxes):
     if len(boxes) == 0 or not boxes:
         print('1')
         return False
-    
+
     visitedBoxes = [0]
     expectedKeys = [i for i in range(len(boxes))]
 
@@ -19,12 +19,11 @@ def canUnlockAll(boxes):
         if len(thisBox) == 0:
             truthVal = (sorted(visitedBoxes) == expectedKeys)
             return truthVal
-        
+
         for boxKey in thisBox:
             if boxKey >= len(boxes):
                 continue
-            if not boxKey in visitedBoxes:
+            if boxKey not in visitedBoxes:
                 visitedBoxes.append(boxKey)
     truthVal = (sorted(visitedBoxes) == expectedKeys)
     return truthVal
-        
