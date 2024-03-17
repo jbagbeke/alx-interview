d8f8848a3677fd1675ef1f7d89fdeca9c344c96d
Joshua Bubune <joshua.legithive@gmail.com>
2024-03-17 18:34:02 +0000

    0x01. Lockboxes Mandatory Task 0

---
+++ 0x01-lockboxes/0-lockboxes.py
@@ -0,0 +1,30 @@
+#!/usr/bin/env python3
+"""
+Locakboxes problem solution
+"""
+
+
+def canUnlockAll(boxes):
+    """
+    Determines if all boxes can be opened
+    """
+    if len(boxes) == 0 or not boxes:
+        print('1')
+        return False
+    
+    visitedBoxes = [0]
+    expectedKeys = [i for i in range(len(boxes))]
+
+    for thisBox in boxes:
+        if len(thisBox) == 0:
+            truthVal = (sorted(visitedBoxes) == expectedKeys)
+            return truthVal
+        
+        for boxKey in thisBox:
+            if boxKey >= len(boxes):
+                continue
+            if not boxKey in visitedBoxes:
+                visitedBoxes.append(boxKey)
+    truthVal = (sorted(visitedBoxes) == expectedKeys)
+    return truthVal
+        
---
+++ 0x01-lockboxes/README.md
@@ -0,0 +1,2 @@
+0x01. Lockboxes
+ PROJECT ALX BACKEND
