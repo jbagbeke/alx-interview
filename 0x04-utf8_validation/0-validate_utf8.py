22df75da689a312bad1eb318f94a571c127a29c9
Joshua Bubune Agbeke <joshua.legithive@gmail.com>
2024-04-05 19:14:58 +0000

    0x04. UTF-8 Validation Mandatory Task 0

--- 0x04-utf8_validation/0-validate_utf8.py
+++ 0x04-utf8_validation/0-validate_utf8.py
@@ -9,10 +9,23 @@ def validUTF8(data):
     Validate utf-8 code encryption
     """
     valid_utf8 = True
-    
-    for val in data:
-        valid_val = val & 0x7F
 
-        if valid_val != val:
+    for val in data:
+        if 0 <= val <= 127:
+            continue
+
+        val_bin = bin(val)
+        val_bin = val_bin[2:]
+
+        if len(val_bin) >= 8:
+            val_bin = val_bin.zfill(8)
+        else:
             valid_utf8 = False
+            continue
+
+        if val_bin[0] == '0' or val_bin[:3] == '110' or \
+                val_bin[:4] == '1110' or val_bin[:5] == '11110':
+            continue
+
+        valid_utf8 = False
     return valid_utf8
