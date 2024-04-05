5dac46782fb4afdf8cbe237caefdd58b3d8fd986
Joshua Bubune Agbeke <joshua.legithive@gmail.com>
2024-04-05 15:04:44 +0000

    0x04. UTF-8 Validation Mandatory Task 0

---
+++ 0x04-utf8_validation/0-validate_utf8.py
@@ -0,0 +1,18 @@
+#!/usr/bin/python3
+"""
+UTF-8 Code Validation
+"""
+
+
+def validUTF8(data):
+    """
+    Validate utf-8 code encryption
+    """
+    valid_utf8 = True
+    
+    for val in data:
+        valid_val = val & 0xFF
+        
+        if valid_val != val:
+            valid_utf8 = False
+    return valid_utf8
